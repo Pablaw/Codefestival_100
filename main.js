21bcf0f588242751e22ed84b8dabce4dbf819aba
Pablaw <chulhwa7@gmail.com>
2021-12-02 14:16:12 +0900

    Dec 1st Thu PM

--- main.js
+++ main.js
@@ -8,14 +8,30 @@ let arr2 = a[1];
 let arr3 = b[0];
 let arr4 = b[1];
 
-let answer = '';
-let arrAnswer =[];
+let arr = '';
+let arrAnswer0 =[];
+let arrAnswer1 =[];
+let answer = [];
 
 for(let i=0; i < a.length; i++) {
     if(arr1[i] * arr3[i] !== 0) {
-        answer = arr1[i] * arr3[i];
-        arrAnswer.push(answer);
+        arr = arr1[i] * arr3[i];
+        arrAnswer0.push(arr);
+    } else if (arr1[i] * arr4[i] !== 0) {
+        arr = arr1[i] * arr4[i];
+        arrAnswer0.push(arr);
     }
 }
-console.log(arrAnswer);
+for(let j=0; j < a.length; j++) {
+    if(arr2[j] * arr3[j] !== 0) {
+        arr = arr2[j] * arr3[j];
+        arrAnswer1.push(arr);
+    } else if (arr2[j] * arr4[j] !== 0) {
+        arr = arr2[j] * arr4[j];
+        arrAnswer1.push(arr);
+    }
+}
+answer.push(arrAnswer0);
+answer.push(arrAnswer1);
+console.log(answer);
 

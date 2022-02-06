aa52b4e44906569e18a046c4e878128d3689616b
pablaw <>
2022-02-06 20:06:17 +0900

    Feb 2nd Sun Night

--- main.js
+++ main.js
@@ -299,3 +299,13 @@ process.stdin.on('data', data => {
     console.log(('*'.repeat(a)+'\n').repeat(b));
 });
 
+// x만큼 간격이 있는 n개의 숫자
+function solution(x, n) {
+    let answer = [];
+    let sum = 0;
+    for(let i=0; i < n; i++) {
+        sum += x;
+        answer.push(sum);
+    }
+    return answer;
+}
